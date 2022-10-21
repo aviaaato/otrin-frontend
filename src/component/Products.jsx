81c62ad9c5635df70ad796de0368cfc2c150edc7
radoheritiana <radoheritiana.rhja.123@gmail.com>
2022-10-21 19:21:56 +0300

    ajout pagination

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import { Link } from "react-router-dom";
 import Paginator from "./Paginator";
+import $ from 'jquery';
 
 const calculate_total_page = (total_items, items_per_page) => {
     var total;
@@ -14,30 +15,45 @@ const calculate_total_page = (total_items, items_per_page) => {
 
 const Products = ({ products }) => {
 
-    //const [paginationConfig, setPaginationConfig] = useState({});
-    //const [current_page, setCurrentPage] = useState({});
+    const items_per_page = 4;
 
-    /*useEffect(() => {
-        let number_of_page = calculate_total_page(products.length, 5);
+    const [paginationConfig, setPaginationConfig] = useState({});
+    const [paginatedProduct, setPaginatedProduct] = useState([]);
+    const [current_page, setCurrentPage] = useState(1);
+    const [total_page, setTotalPage] = useState(1);
+
+    const paginateProduct = (items_per_page, products, page) => {
+      let first_index = (items_per_page*page) - items_per_page;
+      let last_index = items_per_page*page;
+      let paginated_product = products.slice(first_index, last_index);
+      setPaginatedProduct(paginated_product);
+    }
+
+    useEffect(() => {
+        $('.page-link').attr('href', '#result');
+        let number_of_page = calculate_total_page(products.length, items_per_page);
+        setTotalPage(number_of_page);
+        paginateProduct(items_per_page, products, current_page)
         setPaginationConfig({
             totalPages: number_of_page,
             currentPage: current_page,
-            showMax: 2,
+            showMax: 1,
             size: "sm",
             threeDots: true,
             prevNext: true,
             onClick: function (page) {
-                setCurrentPage(page);
-                //getUsers(page);
+              $('.page-link').attr('href', '#result');
+              setCurrentPage(page);
+              paginateProduct(items_per_page, products, page);
             }
         });
-    }, [products])*/
+    }, [current_page, products]);
 
   return (
     <React.Fragment>
-      <ul className="list-group">
-        {products && products.length > 0 ? products.map((p, index) => (
-          <li className="list-group-item d-flex justify-content-evenly mb-2 shadow" key={index}>
+      <ul className="list-group pe-3 ps-3">
+        {paginatedProduct && paginatedProduct.length > 0 ? paginatedProduct.map((p, index) => (
+          <li className="list-group-item rounded-3 pt-0 pb-0 pe-3 d-flex justify-content-start mb-2 shadow" key={index}>
             <div>
               <div className="left-image">
                 <Link href="#">
@@ -45,15 +61,18 @@ const Products = ({ products }) => {
                 </Link>
               </div>
             </div>
-            <div>
-              <h5>{p.product.name}</h5>
-              <h5>{p.product.category.name}</h5>
-              <h5>{p.store.name} {p.store.adresse}</h5>
-              <h5>{p.value} Ar</h5>
+            <div className="ms-3">
+              <p className="fs-6">{p.product.name}<br/>
+              {p.product.category.name}<br/>
+              {p.store.name} {p.store.adresse}<br/>
+              {p.value} Ar</p>
             </div>
           </li>
         )) : <h3>Pas de produits trouvés!</h3>}
       </ul>
+      <div className="d-flex justify-content-center">
+        <Paginator total_page={total_page} paginationConfig={paginationConfig}/>
+      </div>
     </React.Fragment>
   );
 };
--- src/component/Resultat.jsx
+++ src/component/Resultat.jsx
@@ -5,18 +5,18 @@ import Products from "./Products";
 const Resultat = ({prices_filtered}) => {
   return (
     <React.Fragment>
-      <div className="contact-page">
+      <div className="contact-page" id="resultat">
         <div className="container">
           <div className="row">
             <div className="col-lg-12">
               <div className="inner-content">
                 <div className="row">
-                  <div className="col-md-8 p-3">
+                  <div className="col-md-8 pe-3 ps-3 pt-1 pb-1">
                     <div id="map">
                       
                     </div>
                   </div>
-                  <div className="col-md-4 p-3">
+                  <div className="col-md-4 ps-3 pe-3 pt-1 pb-1">
                       <div className="row">
                         <Products products={prices_filtered}/>
                       </div>
